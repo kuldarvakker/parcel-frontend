45d75a4a2013b6030821e5f5035ba3f5bd359e3c
kuldarvakker <kuldarvakker@gmail.com>
2024-03-04 20:15:58 +0200

    chore: remove unused title variable from AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import {Component} from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Angular 16 Crud example';
-}
+export class AppComponent {}
